3ba65d40a8809747752b64e98a70aba947ef64de
tadeyemo32 <tadeyemo32@gmail.com>
2025-08-25 19:17:57 +0800

    init

---
+++ src/client/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include "httplib.h"
+
+
+
+
+int main(){
+
+
+    std::cout<<"Omni Client\n";
+
+    return 0 ;
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "httplib.h"
+#include <nlohmann/json.hpp>
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+    std::cout<<"Server ";
+
+    return 0;
+}
