ba05ce209381d4b50d9d791d5a8feeabe64b1811
tadeyemo32 <tadeyemo32@gmail.com>
2025-08-26 04:57:12 +0800

    logger update

--- src/client/src/main.cpp
+++ src/client/src/main.cpp
@@ -5,21 +5,8 @@
 #include "logger/logger.h"
 
 int main() {
-    // Initialize Logger singleton (logs to default file)
     auto &logger = Log::Logger::getInstance();
-    logger.log("Server started", Log::INFO);
-
-    // Example JSON usage
-    nlohmann::json exampleJson;
-    exampleJson["message"] = "Hello, world!";
-    std::cout << exampleJson.dump(4) << std::endl;
-
-    // Example HTTP server setup (optional)
-    // httplib::Server svr;
-    // svr.Get("/", [](const httplib::Request&, httplib::Response& res){
-    //     res.set_content("Hello from server!", "text/plain");
-    // });
-    // svr.listen("localhost", 8080);
-
+    logger.log("Server started", Log::INFO); 
+  
     return 0;
 }
